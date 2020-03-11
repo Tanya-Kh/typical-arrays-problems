5ef6d9f6e69034d89f1df18f5584d43d3ddbe867
Tanya Khreshchykava <tanya.khreshchykava@gmail.com>
2020-03-11 16:42:47 -0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array !== undefined && array.length) {
+        return Math.min.apply(Math, array);
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array !== undefined && array.length) {
+        return Math.max.apply(Math, array);
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array !== undefined && array.length) {
+        var amount = array.length;
+        var sum = 0;
+        for (var i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        return sum / amount;
+    } else {
+        return 0
+    }
 }
